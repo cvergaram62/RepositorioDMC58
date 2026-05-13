030df27598906b34a3ff581e3626658800ab4996
cvergaram62 <rsvemi86@gmail.com>
2026-05-12 20:05:01 -0500

    Update app.py

--- app.py
+++ app.py
@@ -17,6 +17,13 @@ if sesion == "Sesión 1":
 
 elif sesion == "Sesión 2":
     st.write("Bienvenido a la sesión 2")
+    
+    precio = st.number_input("Ingrese el precio del producto:")
+    descuento = st.number_input("Ingrese el descuento del producto:")
+
+    precio_final_producto = precio - (precio*descuento)
+
+    st.write("El precio final del producto es: ",precio_final_producto"
 
 elif sesion == "Sesión 3":
     st.write("Bienvenido a la sesión 3")
