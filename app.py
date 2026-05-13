d4b58e539ce30672876078bf5ebfed90fc0c6cef
cvergaram62 <rsvemi86@gmail.com>
2026-05-12 19:48:21 -0500

    Update app.py

--- app.py
+++ app.py
@@ -5,3 +5,5 @@ st.title("Mi primera aplicación en Python")
 st.sidebar.title("Parámetros")
 
 st.write("Elaborado por: Cristhian Vergara Miranda")
+
+sesion = st.selectbox("Seleccione una sesión", ["Sesión 1","Sesión 2","Sesión 3","Sesión 1"])
